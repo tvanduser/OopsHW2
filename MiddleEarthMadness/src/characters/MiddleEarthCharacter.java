6812bc9fc896c5ab4382559b8ad57d8d3a4341bb
Turner Van Duser <turner.vanduser@gmail.com>
2025-03-04 00:16:26 -0600

    Update MiddleEarthCharacterClass
    Added the: attributes, getters and setters, as well as the abstract methods, started on the concrete method display books

--- MiddleEarthMadness/src/characters/MiddleEarthCharacter.java
+++ MiddleEarthMadness/src/characters/MiddleEarthCharacter.java
@@ -2,4 +2,54 @@ package characters;
 
 public abstract class MiddleEarthCharacter {
 	
+	private String name;
+	private double health;
+	private double power;
+	
+	//constructor 
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	
+	//----------getters and setters-----------------
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHealth() {
+		return health;
+	}
+
+	public void setHealth(double health) {
+		this.health = health;
+	}
+
+	public double getPower() {
+		return power;
+	}
+
+	public void setPower(double power) {
+		this.power = power;
+	}
+	
+	//----------abstract methods-------------- 
+	abstract boolean attack(MiddleEarthCharacter target);
+
+	//returns the race of a character
+	abstract String getRace();
+	
+	
+	//---------concrete methods---------------
+	//displays character info 
+	void displayInfo() {
+		//TODO finish this thing
+	}
+	
+	
 }
