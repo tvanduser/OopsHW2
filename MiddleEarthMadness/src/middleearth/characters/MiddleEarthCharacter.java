e37759d61e6794209afaa2cb77f8215f3b71fda4
Turner Van Duser <turner.vanduser@gmail.com>
2025-03-05 23:08:13 -0600

    Update MiddleEarthCharacter.java
    added javadocs

--- MiddleEarthMadness/src/middleearth/characters/MiddleEarthCharacter.java
+++ MiddleEarthMadness/src/middleearth/characters/MiddleEarthCharacter.java
@@ -9,6 +9,12 @@ public abstract class MiddleEarthCharacter {
 	private double power;
 	
 	//constructor 
+	/**
+	 * constructor when you make a new character to set its attributes 
+	 * @param name of character
+	 * @param health of character
+	 * @param power of character
+	 */
 	public MiddleEarthCharacter(String name, double health, double power) {
 		this.name = name;
 		this.health = health;
@@ -16,41 +22,76 @@ public abstract class MiddleEarthCharacter {
 	}
 	
 	//----------getters and setters-----------------
+	/**
+	 * returns the name of a character
+	 * @return name of character
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * sets the name of a middle earth character
+	 * @param name set the name of the character
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * gets the health of a character
+	 * @return health of character
+	 */
 	public double getHealth() {
 		return health;
 	}
 
+	/**
+	 * sets the health of a character
+	 * @param health of character
+	 */
 	public void setHealth(double health) {
 		this.health = health;
 	}
 
+	/**
+	 * gets the power of a character
+	 * @return the power of a character
+	 */
 	public double getPower() {
 		return power;
 	}
 
+	/**
+	 * sets the power of a character
+	 * @param power of character
+	 */
 	public void setPower(double power) {
 		this.power = power;
 	}
 	
 	//----------abstract methods-------------- 
+	/**
+	 * abstract function that acts differently depending on what character type is attacking 
+	 * @param target the character being attacked 
+	 * @return true on success false on fail 
+	 */
 	public abstract boolean attack(MiddleEarthCharacter target);
 
 	//returns the race of a character
+	/**
+	 * abstract method to return the race of a character
+	 * @return race of character 
+	 */
 	public abstract String getRace();
 	
 	
 	//---------concrete methods---------------
 	//displays character info 
+	/**
+	 * displays the name, health, and power of a character
+	 */
 	public void displayInfo() {
-		//TODO finish this thing
 		System.out.println("Name: " + this.getName() + ", Health: " + this.getHealth() + ", Power: " + this.getPower());
 	}
 	
